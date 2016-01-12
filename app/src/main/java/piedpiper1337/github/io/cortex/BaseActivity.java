feff70c0d7e6f121cd4f75360a2d394907a6c464
Brian Zhao <bzspartan@gmail.com>
2016-01-11 21:44:13 -0800

    Basic SMS intent and Color Change

--- app/src/main/java/piedpiper1337/github/io/cortex/BaseActivity.java
+++ app/src/main/java/piedpiper1337/github/io/cortex/BaseActivity.java
@@ -1,13 +1,17 @@
 package piedpiper1337.github.io.cortex;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -22,10 +26,17 @@ public class BaseActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                String phoneNumber = "1234567890";
+                String smsBody = "Testing";
+                Uri uri = Uri.parse("smsto:" + phoneNumber);
+                Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+                it.putExtra("sms_body", smsBody);
+                startActivity(it);
             }
         });
+
     }
 
     @Override
